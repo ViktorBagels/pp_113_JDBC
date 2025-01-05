52289b8cbbcab731f2df514844228696508d43c2
ViktorBagels <viktorrozhkov@gmail.com>
2025-01-05 06:12:24 +0300

    Initial commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,23 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Ivan", "Ivanov", (byte) 25);
+        userService.saveUser("Petr", "Petrov", (byte) 30);
+        userService.saveUser("Anna", "Smirnova", (byte) 22);
+        userService.saveUser("Olga", "Sidorova", (byte) 27);
+
+        userService.getAllUsers().forEach(System.out::println);
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();// реализуйте алгоритм здесь
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,42 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+
+    private final UserDao userDao = new UserDaoJDBCImpl(); // DAO-слой для взаимодействия с базой
+
+    @Override
     public void createUsersTable() {
-
+        userDao.createUsersTable(); // Перенаправляет вызов к DAO
     }
 
+    @Override
     public void dropUsersTable() {
-
+        userDao.dropUsersTable(); // Перенаправляет вызов к DAO
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        userDao.saveUser(name, lastName, age); // Перенаправляет вызов к DAO
     }
 
+    @Override
     public void removeUserById(long id) {
-
+        userDao.removeUserById(id); // Перенаправляет вызов к DAO
     }
 
+    @Override
     public List<User> getAllUsers() {
-        return null;
+        return userDao.getAllUsers(); // Перенаправляет вызов к DAO и возвращает результат
     }
 
+    @Override
     public void cleanUsersTable() {
-
+        userDao.cleanUsersTable(); // Перенаправляет вызов к DAO
     }
 }
